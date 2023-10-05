12ae3a376c65b7f5f29ed0095a6ae6e9567e56a2
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-04 20:24:34 -0400

    +temperature.py

---
+++ temperature.py
@@ -0,0 +1,3 @@
+tempincelsius = input("Please enter a temperature in Celsis: ")
+tempinfahrenheit = float(tempincelsius) * 1.8  + 32
+print("The temperature is " + str(tempinfahrenheit) + " degrees F.")
